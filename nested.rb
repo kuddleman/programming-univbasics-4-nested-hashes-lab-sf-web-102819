83888e3cc72eaf7c39f0367b7d042798556ef0d0
Donald Lobree <edl1155@gmail.com>
2019-09-13 18:25:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-  programmatically[:dennis_ritchie][:languages]
+  programmatically[:dennis_ritchie][:languages][0]
 
 end
 
